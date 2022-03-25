2130d61d840dad6fb601df1ce3cba42245e357e4
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-25 11:44:42 -0600

    signup.js now functional

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,8 @@
 const loginFormHandler = async function (event) {
-  console.log("testing")
+  // console.log("testing")
+
   event.preventDefault();
+
   try {
     const usernameEl = document.querySelector("#emailField");
     const passwordEl = document.querySelector("#passwordField");
@@ -13,40 +15,23 @@ const loginFormHandler = async function (event) {
       }),
       headers: { "Content-Type": "application/json" },
     });
-    const data = await response.json();
-    console.log(data);
+
+    // const data = await response.json();
+    // console.log(data);
+
     if (response.ok) {
       document.location.replace('/dashboard'); //if (200) 
     } else {
       alert("Failed to login");
     }
+
   } catch (err) {
     console.log(err);
   }
 };
-const signinBtn = document.querySelector("#signinBtn")
 
-// document 
+const signinBtn = document.querySelector("#signinBtn");
+
 if (signinBtn) {
   signinBtn.addEventListener("click", loginFormHandler);
-}
-  // .querySelector("#signinBtn")
-  // .addEventListener("click", loginFormHandler);
-
-// const usernameEl = document.querySelector('#emailField');
-// const passwordEl = document.querySelector('#passwordField');
-
-// const response = await fetch('/api/user/login', {
-//   method: 'POST',
-//   body: JSON.stringify({
-//     username: usernameEl.value,
-//     password: passwordEl.value,
-//   }),
-//   headers: { 'Content-Type': 'application/json' },
-// });
-
-// if (response.ok) {
-//   document.location.replace('/dashboard');
-// } else {
-//   alert('Failed to login');
-// }
+};
---
+++ public/js/signup.js
@@ -0,0 +1,38 @@
+const signUpFormHandler = async function (event) {
+    event.preventDefault();
+
+    try {
+      const firstNameEl = document.querySelector("#firstNameField");
+      const lastNameEl = document.querySelector("#lastNameField");
+      const emailEl = document.querySelector("#emailField");
+      const passwordEl = document.querySelector("#passwordField");
+  
+      const response = await fetch("/api/user/signup", {
+        method: "POST",
+        body: JSON.stringify({
+          name: `${firstNameEl.value} ${lastNameEl.value}`,
+          email: emailEl.value,
+          password: passwordEl.value,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+    //   const data = await response.json();
+    //   console.log(data);
+
+      if (response.ok) {
+        document.location.replace('/dashboard'); 
+      } else {
+        alert("Failed to signup");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const signUpBtn = document.querySelector("#signUpBtn");
+  
+  if (signUpBtn) {
+    signUpBtn.addEventListener("click", signUpFormHandler);
+  };
