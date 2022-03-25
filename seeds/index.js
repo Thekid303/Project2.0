7fe5d39898c1675358cb804213d9cd0ff4adfc13
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-03-24 21:09:52 -0600

    updated dashboard main page

--- seeds/index.js
+++ seeds/index.js
@@ -8,14 +8,14 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
   await seedProjects();
   console.log('\n----- PROJECTS SEEDED -----\n');
 
   await seedComments();
   console.log('\n----- COMMENTS SEEDED -----\n');
-
-  await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
 
   process.exit(0);
 };
