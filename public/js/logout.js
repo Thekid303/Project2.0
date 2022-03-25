21f80af729e29ed9508e135f5e419096d0fee849
Thekid303 <nicholasgraves4@icloud.com>
2022-03-24 18:41:22 -0600

    fixed signin/ log in page

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,30 @@
-const logout = async () => {
+// const logout = async () => {
 
-    // Logs out user 
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+//     // Logs out user 
+//     const response = await fetch('/api/users/logout', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+  
+//     if (response.ok) {
+//       document.location.replace('/');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   };
+  
+//   document.querySelector('#logout').addEventListener('click', logout);
+  
+const logoutBtn = document.querySelector("#logoutBtn");
+
+function logoutUser() {
+    fetch("/users/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
     });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
+    location.href = "/";
+}
+
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", logoutUser);
+}
