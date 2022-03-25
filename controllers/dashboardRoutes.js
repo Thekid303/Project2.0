aa085f867259be9ca00ba99dc3cda691cf4f43ac
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-03-25 11:17:02 -0600

    updated folders

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res) => {
       ],
     });
 
-    const employee = userData.map(employee =>
+    const employee = userData.map((employee) =>
       employee.get({ plain: true })
     );
 
