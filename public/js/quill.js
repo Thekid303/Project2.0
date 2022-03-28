cb375dca68fcfa5cb3304df33f664c696203ad6c
Thekid303 <nicholasgraves4@icloud.com>
2022-03-27 19:12:49 -0600

    working on quill

--- public/js/quill.js
+++ public/js/quill.js
@@ -1,12 +1,15 @@
-var quill = new Quill('#editor', {
-  modules: {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline'],
-      ['image', 'code-block']
-    ]
-  },
-  placeholder: 'Compose an epic project...',
-  theme: 'snow'  
-});
+// var quill = new Quill('#editor', {
+//   modules: {
+//     toolbar: [
+//       [{ header: [1, 2, false] }],
+//       ['bold', 'italic', 'underline'],
+//       ['image', 'code-block']
+//     ]
+//   },
+//   placeholder: 'Compose an epic project...',
+//   theme: 'snow'  
+// });
 
+var quill = new Quill('#editor', {
+    theme: 'snow'
+  });
