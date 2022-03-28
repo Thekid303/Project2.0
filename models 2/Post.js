2fc7df29f665d256e262d547543e02ddcfa59c83
Thekid303 <nicholasgraves4@icloud.com>
2022-03-27 22:39:14 -0600

    working on stuff

---
+++ models 2/Post.js
@@ -0,0 +1,20 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING,
+    boss: DataTypes.STRING, 
+    rate: DataTypes.INTEGER, 
+    date: DataTypes.STRING,
+
+  },
+  {
+    sequelize
+  }
+);
+
+module.exports = Post;
