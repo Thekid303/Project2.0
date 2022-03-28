be09f48a8ff371d5ae5b4f6c0e7e4861926d7d60
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-03-27 21:13:36 -0600

    cleaned up ui

--- public/js/edit.js
+++ public/js/edit.js
@@ -5,7 +5,7 @@ const editFormHandler = async function(event) {
 
   const title = document.querySelector('input[name="post-title"]').value;
   const body = document.querySelector('textarea[name="post-body"]').value;
-  const date = document.querySelector('date[name="post-date"]').value;
+  
 
 
   await fetch(`/api/post/${postId}`, {
@@ -13,8 +13,6 @@ const editFormHandler = async function(event) {
     body: JSON.stringify({
       title,
       body, 
-      date,
-    
     }),
     headers: {
       'Content-Type': 'application/json'
@@ -22,10 +20,6 @@ const editFormHandler = async function(event) {
   });
 
   document.location.replace('/dashboard');
-
-
-
-
 };
 
 const deleteClickHandler = async function() {
